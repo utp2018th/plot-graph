eaf6c611a9cb2154c9b7b21cd6cab3b0d55832f3
Seika139 <tcoanzu@gmail.com>
2019-06-21 00:24:42 +0900

    こまめなコミット

--- plot_master2.py
+++ plot_master2.py
@@ -10,7 +10,7 @@ def sig_dig(float):
     pass
 
 def get_array_from_data(path):
-    input_mode = input('csv â†’ c tsv â†’ t else â†’ text \n>> ')
+    input_mode = input('読み込むファイルが\ncsvなら「c」を、tsvなら「t」を、どちらでもない場合はそれ以外の入力してください\n>> ')
     with open(path) as f:
         if input_mode == "c":
             reader = csv.reader(f)
@@ -50,27 +50,39 @@ class graph_master:
             y.get_proportion(x.values,y.values)
         a = y.proportion
         y1 = a * x.values
-        label = 'y=' + str(round(a,4)) + 'x+'
-        plt.plot(x.values,y1,c=y.color,label=)
+        equation = 'y=' + str(round(a,y.sig_dig)) + 'x'
+        corr = 'R={}'.format(round(np.corrcoef(x.values,y.values)[0][1],y.sig_dig))
+        text = equation + '\n' + corr
+        plt.plot(x.values,y1,c=y.color,label=text)
+
+    def plot_liner(self,x,y):
+        if not y.linear:
+            y.get_linear(x.values,y.values)
+        a,b = y.linear
+        y1 = a * x + b
+        equation = 'y=' + str(round(a,y.sig_dig)) + 'x+' str(round(b,y.sig_dig))
+        corr = 'R={}'.format(round(np.corrcoef(x.values,y.values)[0][1],y.sig_dig))
+        text = equation + '\n' + corr
+        plt.plot(x.values,y1,c=y.color,label=text)
 
     def make_graph(self,var_x,*var_y):
         plt.figure(figsize=(9,6),dpi=128)
         plt.title(self.title)
         plt.xlabel(self.xlabel)
         plt.ylabel(self.ylabel)
+        input_text = 'もし比例の近似直線が欲しいなら1を、線形の近似直線が欲しいなら2を、'
+        input_text += '近似直線が不要な場合はそれ以外の入力をしてください\n>> '
+        ans = input(input_text)
         for y in var_y:
             plt.scatter(var_x.values,y.values,label=y.label,color=y.color)
+            if ans ==  "1":
+                self.plot_proportion(var_x,var_y)
+            elif ans == "2":
+                self.plot_liner(var_x,var_y)
         plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=9)
         plt.subplots_adjust(right=0.77)
         plt.grid()
 
-def test(a,*b):
-    print(a)
-    for i in b:
-        print(i)
-    print(b)
-
 if __name__ == "__main__":
     path = 'input/0620.csv'
     get_array_from_data(path)
-    test(1,2,3,4,5)
